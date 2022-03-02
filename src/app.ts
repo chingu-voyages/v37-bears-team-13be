91fe184af86c90820f4eb09ce58bc93adee9b36a
Richard Kwon <rkminjae@gmail.com>
2022-03-02 14:22:16 -0500

    Feat: add get all users route

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { json } from 'body-parser';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import { signupRouter } from './routes/signup';
+import { usersRouter } from './routes/users';
 
 // Initialize express.
 const app = express();
@@ -20,6 +21,7 @@ app.use(helmet());
 
 // Add routes.
 app.use(signupRouter);
+app.use(usersRouter);
 
 // Export the app and environment variables.
 export { app, NODE_ENV, PORT, ORIGIN, MONGO_URI, JWT_KEY };
